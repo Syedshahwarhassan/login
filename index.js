e9aa21b94c26d61670064baf0d7a28215fd673c0
Syedshahwarhassan <Syedshahwarhassan87@gmail.com>
2024-01-10 11:52:16 +0500

    update

--- index.js
+++ index.js
@@ -21,36 +21,49 @@ if(pass.length = 6){
     swal("Error","Password have max 6 Character", "error");
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function loginbtn() {
+        var y=document.getElementById("login-back-");
+    if(y.style.display=="none"){
+        y.style.display="block";
+    }else{
+        y.style.display="block";
+    }
   var x = document.getElementById("sign-form-");
   if (x.style.display == "none") {
     x.style.display = "block";
+    document.getElementById("login-back-").style.height="20%";
+    document.getElementById("login-back-").style.width="20%";
+    // document.getElementById("login-back-").style.backgroundColor="rgba(0,0,0,0.6)";
     document.getElementById("login-form-").style.display = "none";
     document.getElementById("loginbtn").style.backgroundColor = "white";
     document.getElementById('loginbtn').innerHTML="Sign In";
+    document.getElementById('cancelbtn').style.display="block";
+
+
   } else {
+    var y=document.getElementById("login-back-");
+    if(y.style.display=="none"){
+        y.style.display="block";
+    }else{
+        y.style.display="block";
+    }
+    var y=document.getElementById("login-back-").style.display="block;";
     x.style.display = "none";
 
     document.getElementById("login-form-").style.display = "block";
     document.getElementById("loginbtn").style.backgroundColor = "lightgray";
     document.getElementById('loginbtn').innerHTML="Login";
-  }
+    document.getElementById('cancelbtn').style.display="block";
+    
 }
+}
+function cancelbtn() {
+    document.getElementById("login-back-").style.display="none";
+    
+}
+
+
+
 // function loginbtn() {
 //     var x = document.getElementById("login-form-");
 //     if (x.style.display == "none") {
